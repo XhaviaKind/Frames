7369a841231f14857bd75a065e03f8ea7b240fbc
2ndyrGroupA <honkBlind@gmail.com>
2019-12-01 17:03:44 +0800

    Initial commit

---
+++ src/DB/NOTES.java
@@ -0,0 +1,8 @@
+// Create a database butanon;
+// Create table users (ID int AUTO_INCREMENT, Firstname varchar(50) NOT NULL, Lastname varchar(50) NOT NULL, 
+    // Username varchar(50) NOT NULL, Password varchar(50) NOT NULL, Role varchar(500) NOT NULL, PRIMARY KEY(ID));
+// Create table medicine (ID int AUTO_INCREMENT, Med_for varchar(50) NOT NULL, GenericName varchar(50) NOT NULL, 
+    // BrandName varchar(50) NOT NULL, Price double NOT NULL, Quantity int NOT NULL, Description varchar(500) NOT NULL,
+    //PRIMARY KEY(ID));
+// create table purchase_history (ID int AUTO_INCREMENT,GenericName varchar(50) NOT NULL ,BrandName varchar(60) NOT NULL,Price double NOT NULL,
+    // Quantity INT NOT NULL, Description VARCHAR(50) NOT NULL, Total_Amount DOUBLE NOT NULL, PRIMARY KEY(ID));
